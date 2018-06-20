b536934c26321a3d71effcc88d07b73615ffa00d
yuecf@bsoft.com.cn <yuecf@bsoft.com.cn>
2018-06-20 09:27:46 +0800

    测试

--- YCFDatePicker/YCFDatePicker/ViewController.m
+++ YCFDatePicker/YCFDatePicker/ViewController.m
@@ -24,6 +24,47 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     YCFDatePicker *dp = [[YCFDatePicker alloc] init];
+    dp.isCanAutorotate = YES;
+    dp.typeDatePicker = YCFDatePickerType_DateAndTime;
+    [dp showWithSourceVC:self completion:^{
+        
+    }];
+}
+
+-(IBAction)action1:(id)sender {
+    YCFDatePicker *dp = [[YCFDatePicker alloc] init];
+    dp.isCanAutorotate = YES;
+    dp.typeDatePicker = YCFDatePickerType_Date;
+    [dp showWithSourceVC:self completion:^{
+        
+    }];
+}
+
+-(IBAction)action2:(id)sender {
+    YCFDatePicker *dp = [[YCFDatePicker alloc] init];
+    dp.isCanAutorotate = YES;
+    dp.normalColorCancelButton = [UIColor orangeColor];
+    dp.dateMax = [NSDate date];
+    dp.typeDatePicker = YCFDatePickerType_DateAndTime;
+    [dp showWithSourceVC:self completion:^{
+        
+    }];
+}
+
+-(IBAction)action3:(id)sender {
+    YCFDatePicker *dp = [[YCFDatePicker alloc] init];
+    dp.isCanAutorotate = YES;
+    dp.normalColorSureButton = [UIColor greenColor];
+    dp.dateMin = [NSDate date];
+    dp.typeDatePicker = YCFDatePickerType_DateAndTime;
+    [dp showWithSourceVC:self completion:^{
+        
+    }];
+}
+
+-(IBAction)action4:(id)sender {
+    YCFDatePicker *dp = [[YCFDatePicker alloc] init];
+    dp.isCanAutorotate = YES;
     dp.typeDatePicker = YCFDatePickerType_DateAndTime;
     [dp showWithSourceVC:self completion:^{
         
@@ -33,6 +74,5 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
-
 
 @end
